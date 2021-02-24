22206e4c70e1a2d2236435988d9d47a80631822e
a3at28 <a3at28@gmail.com>
2021-02-24 22:41:45 +0300

    BodeMassIndexService.java added

--- src/main/java/BodeMassIndexService.java
+++ src/main/java/BodeMassIndexService.java
@@ -1,2 +1,7 @@
 public class BodeMassIndexService {
+    int calculate (double massKg, double heightSm) {
+        double heightM = heightSm / 100;
+        double bodyMassIndex = massKg / (heightM * heightM);
+        return (int)bodyMassIndex;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(123);
+        BodeMassIndexService service = new BodeMassIndexService();
+        int bodyMassIndex = service.calculate(69.5,168.5);
+        System.out.println(bodyMassIndex);
+
+
     }
 }
